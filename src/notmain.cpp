a1b4b414216f2248daec7071aca72787a58890c4
Veronika Sevastyanova <veronika.sewastyanova@yandex.ru>
2026-09-23 20:59:46 +0300

    Initial commit: first version of the project

---
+++ src/notmain.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 1;
+    while (i <= 100){
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
+
+
+
+// #include <iostream>
+// #define s(x)(2.5*x)
+// using namespace std;
+// int main(){
+//     for (int i = 1; i < 21; i++){
+//         cout << i << " дюйм = " << s(i) << "см\n";
+//     }
+//     return 0;
+// }
+
